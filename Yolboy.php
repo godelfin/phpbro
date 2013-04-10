5fda2dd68eeb3b813097d27a29f74924638376ee
godelfin <tovar.brian@gmail.com>
2013-04-09 17:45:39 -0700

    Create Yolboy.php

---
+++ Yolboy.php
@@ -0,0 +1,9 @@
+<?
+/**
+ * You Only Live Bro Once, Yo
+ *
+ * A PHPBro Library. Stores static methods into session, which can then be safely executed anywhere in your code.
+ *
+ **/
+class Yolboy extends BroGuy {
+}
